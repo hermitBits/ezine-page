8e89a9c61e540991d9b7aec14bdc820494309359
Mateus Camargo <mateuscamargo0@outlook.com.br>
2023-09-22 19:11:36 -0300

    feat(conteudos): dedicando arquivo para receber arquivos de anotações

---
+++ conteudos/arquivos.js
@@ -0,0 +1,23 @@
+const arquivos = [
+    {
+        href: "...",
+        titulo: "Título",
+        autor: "Autor",
+        criado: "Data",
+        ativo: false
+    },
+    {
+        href: "conteudos/2023-09/21/01-configuraca-conexao-ssh.txt.asc",
+        titulo: '01 - Arquivo de configurações para facilitar conexão SSH',
+        autor: 'hermitBits',
+        criado: '21/09/2023',
+        ativo: true
+    },
+    {
+        href: "conteudos/2023-09/22/02-systemd-resolved-dns-cache.txt",
+        titulo: '02 - DNS Cache com systemd-resolved',
+        autor: 'hermitBits',
+        criado: '22/09/2023',
+        ativo: true
+    }
+]
